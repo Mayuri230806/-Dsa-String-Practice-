bff2ec96bcbc1973c877dd7511be9dbc3219a7b9
Mayuri Mangal <mayurimangal79@gmail.com>
2026-07-26 11:25:19 +0530

    Add unique character finder in uniquechar.cpp
    Implement a program to find and print unique characters in a string.

---
+++ uniquechar.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s="aabc";
+    int freq[256]={0};
+    
+    for(int i=0;i<s.length();i++){
+        
+        freq[s[i]]++;
+    }
+    cout<<"the unique value are->";
+     for(int i = 0; i < s.length(); i++)
+    {
+        if(freq[s[i]]==1){
+            cout << s[i] ;
+            freq[s[i]] = 0; 
+    }
+}
+    return 0;
+}
